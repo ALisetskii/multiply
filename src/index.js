e6bb87d293eac6224df603f68ca2c59ab7aa205b
AListckii <sobbabobba@gmail.com>
2019-10-09 23:02:35 +0400

    feat: add multiply solution
    multiply solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var firstArr = first.split('').reverse();
+  var secondArr = second.split('').reverse();
+
+  var result = [];
+
+  for (var i = 0; i < firstArr.length; i++) {
+    for (var j = 0; j < secondArr.length; j++) {
+      var mult = firstArr[i] * secondArr[j];
+      if (result[i + j])
+      {
+        result[i + j] = result[i + j] + mult;
+      } else {
+        result[i + j] = mult;
+      }
+      
+    }
+  }
+
+  for (var i = 0; i < result.length; i++) {
+    var num = result[i] % 10;
+    var move = Math.floor(result[i] / 10);
+    result[i] = num;
+
+    if (result[i + 1])
+      result[i + 1] += move;
+    else if (move != 0)
+      result[i + 1] = move;
+  }
+
+
+  return result.reverse().join('');
 }
